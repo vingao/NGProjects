78334d2b48d60bacb2bed92743e849143dab0467
unknown <a202898@LA974014.dmn1.fmr.com>
2015-09-20 13:32:58 -0400

    add NGUIRouterLevels

---
+++ NGUIRouterLevels/js/main.js
@@ -0,0 +1,52 @@
+var app = angular.module('demo', ['ui.router']);
+
+app.config(['$stateProvider', '$urlRouterProvider', function ($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.otherwise('/');
+
+    $stateProvider
+        .state('users', {
+            url: '/users',
+            templateUrl: 'templates/users.html'
+        })
+        .state('users.profile', {
+            url: '/profile',
+            templateUrl: 'templates/profile.html'
+        })
+        .state('users.profile.photo', {
+            url: '/profile',
+            templateUrl: 'templates/photo.html'
+        })
+        .state('users.account', {
+            url: '/account',
+            templateUrl: 'templates/account.html'
+        });
+}])
+
+app.controller('demoCtrl', function ($scope, $state) {
+    $scope.goto = function (page) {
+        $state.go(page, {replace: true});
+    }
+
+    $scope.commands = ['users', 'users.profile', 'users.profile.photo', 'users.account'];
+    $scope.currentIndex = 0;
+    $scope.command = $scope.commands[$scope.currentIndex];
+
+    $scope.$watch('command', function (newValue, oldValue) {
+        //console.log('new and old commands: ', newValue, oldValue);
+        if (newValue != oldValue) {
+            $scope.goto(newValue);
+        }
+    })
+
+    $scope.sendCommand = function() {
+
+        setInterval(function(){
+            //console.log("current command and index", $scope.command, $scope.currentIndex);
+            $scope.currentIndex = ($scope.currentIndex++) < $scope.commands.length-1?$scope.currentIndex++:0;
+            $scope.command = $scope.commands[$scope.currentIndex];
+            //console.log("new command and index", $scope.command, $scope.currentIndex);
+            $scope.$digest();
+        }, 3000);
+    }
+    $scope.sendCommand();
+})
